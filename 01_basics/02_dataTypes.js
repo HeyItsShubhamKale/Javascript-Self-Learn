9bdb8416eded3f21fd2355caf88d2e4f2f9325c3
Shubham Dnyaneshwar Kale <74854692+HeyItsShubhamKale@users.noreply.github.com>
2023-12-28 08:03:07 +0000

    types of data

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // Treats all JS code as a newer version of JS
+//alert(3+3) // We are using nodejs, not browser
+// code readability should be high
+
+
+
+// PRIMITIVE DATA-TYPE
+//number => 2 to power 53 size
+//bigint = size greater than default int size which is 2 to power 53
+//String => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value has not been assigned
+// Symbol => unique
+
+
+
+//Object
+
+console.log(typeof undefined ) // O/P undefined
+console.log(typeof null); // o/p Object
